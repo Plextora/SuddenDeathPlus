8a4e235834af00127ad730d6119f77fabd6d8117
Plextora <71889427+Plextora@users.noreply.github.com>
2025-03-25 22:38:49 -0400

    Add signatures and offsets (thanks tosu project!)

--- OsuSignatures.cs
+++ OsuSignatures.cs
@@ -1,12 +1,16 @@
-﻿namespace SuddenDeathPlus
+﻿// Signatures and offsets grabbed from https://github.com/tosuapp/tosu, developed by cyperdark, KotRikD, and other contributors.
+
+namespace SuddenDeathPlus
 {
     class Signatures
     {
-
+        public string gameStatus => "48 83 F8 04 73 1E";
+        public string modsEnabled => "C8 FF ?? ?? ?? ?? ?? 81 0D ?? ?? ?? ?? ?? 08 00 00";
     }
 
     class Offsets
     {
-
+        public uint gameStatus => 0x04;
+        public uint modsEnabled => 0x09;
     }
 }
