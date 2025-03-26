e1308ab320e28ce05cfc09c4f10f2912528b25f3
Plextora <71889427+Plextora@users.noreply.github.com>
2025-03-26 13:46:17 -0400

    Code for detecting miss with SD (no BSOD code)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿namespace SuddenDeathPlus
+﻿using System.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace SuddenDeathPlus
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            OsuMemory osuMemory = new();
+
+            Console.WriteLine("Waiting for game start...");
+            while (true)
+            {
+                int gameStatus = await osuMemory.GetGameStatus();
+                int modsEnabled = await osuMemory.GetModsEnabled();
+                short hitMiss = await osuMemory.GetMisses();
+
+                if (gameStatus == 2 && (modsEnabled & 32) != 0 && hitMiss >= 1 && hitMiss != 27136)
+                {
+                    Console.WriteLine("Fatal mistake..");
+                    Thread.Sleep(3000);
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
